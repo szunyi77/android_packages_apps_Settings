1e4a2b41346a55775f6ac29d56c96f48607adf20
Loic Poulain <loicx.poulain@intel.com>
2013-07-10 11:22:43 +0200

    WiFi: Fix Null Pointer Exception in Wifi Settings dialog
    This patch checks that the activity remains available in the
    dialog onclick callback before using it.
    
    Change-Id: I1886b87498455e890962415a864da803c94561a4
    Orig-Change-Id: I1d7aba1b43a00568dcfaf580e7082aadc12ebde5
    Signed-off-by: Loic Poulain <loicx.poulain@intel.com>
    Signed-off-by: Jian Luo <jian.luo@intel.com>
    Signed-off-by: Bruce Beare <bruce.j.beare@intel.com>
    Signed-off-by: Jack Ren <jack.ren@intel.com>
    Author-tracking-BZ: 122149

--- src/com/android/settings/wifi/WifiSettings.java
+++ src/com/android/settings/wifi/WifiSettings.java
@@ -670,8 +670,11 @@ public class WifiSettings extends SettingsPreferenceFragment
                                     new DialogInterface.OnClickListener() {
                                 @Override
                                 public void onClick(DialogInterface dialog, int id) {
-                                    getActivity().setResult(RESULT_SKIP);
-                                    getActivity().finish();
+                                    Activity activity = getActivity();
+                                    if (activity != null) {
+                                        activity.setResult(RESULT_SKIP);
+                                        activity.finish();
+                                    }
                                 }
                             })
                             .setPositiveButton(R.string.wifi_dont_skip,
@@ -689,8 +692,11 @@ public class WifiSettings extends SettingsPreferenceFragment
                                     new DialogInterface.OnClickListener() {
                                 @Override
                                 public void onClick(DialogInterface dialog, int id) {
-                                    getActivity().setResult(RESULT_SKIP);
-                                    getActivity().finish();
+                                    Activity activity = getActivity();
+                                    if (activity != null) {
+                                        activity.setResult(RESULT_SKIP);
+                                        activity.finish();
+                                    }
                                 }
                             })
                             .setPositiveButton(R.string.wifi_dont_skip,
