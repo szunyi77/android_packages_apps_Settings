cdfc13890e6b4294e88908ac9f77c0201917a5b5
Mahdi-Rom <mahdi.rom@gmail.com>
2013-08-29 01:32:55 +0100

    Quick Settings: Clean up Torch tile (1 of 2)

--- src/com/android/settings/slim/quicksettings/QuickSettingsUtil.java
+++ src/com/android/settings/slim/quicksettings/QuickSettingsUtil.java
@@ -57,7 +57,8 @@ public class QuickSettingsUtil {
     public static final String TILE_SLEEP = "toggleSleepMode";    
     public static final String TILE_LTE = "toggleLte";
     public static final String TILE_PROFILE = "toggleProfile";
-    public static final String TILE_REBOOT = "toggleReboot";    
+    public static final String TILE_REBOOT = "toggleReboot"; 
+    public static final String TILE_TORCH = "toggleFlashlight";   
 
     private static final String TILE_DELIMITER = "|";
     private static final String TILES_DEFAULT = TILE_USER
@@ -129,6 +130,9 @@ public class QuickSettingsUtil {
 	TILES.put(TILE_SYNC, new QuickSettingsUtil.TileInfo(
                 TILE_SYNC, R.string.title_tile_sync,
                 "com.android.systemui:drawable/ic_qs_sync_off"));
+	TILES.put(TILE_TORCH, new QuickSettingsUtil.TileInfo(
+                TILE_TORCH, R.string.title_tile_torch,
+                "com.android.systemui:drawable/ic_qs_torch_off"));
 		
 // These toggles are not available yet.  Comment out for now
 //        TILES.put(TILE_SCREENTIMEOUT, new QuickSettingsUtil.TileInfo(
