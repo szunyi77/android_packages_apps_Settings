40b72d15012bf79781a60bc1e675ed6b7e2db91e
Jorrit Jongma <git@jongma.org>
2014-01-17 10:41:41 +0100

    Launch SuperSU from Settings
    If SuperSU is installed, show an entry in Settings below
    "Developer options", which launches SuperSU when tapped.
    At boot time SuperSU will detect if com.android.settings
    contains the resources from this patch, and if so it
    will hide itself from the launcher. This was implemented
    in v1.85, so we only show the entry if the installed
    SuperSU version is at least that.
    
    Change-Id: I1796d92458587b9e961d5d8e6d58099e8367834d

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -648,6 +648,16 @@ public class Settings extends PreferenceActivity
                         target.remove(i);
                     }
                 }
+            } else if (id == R.id.supersu_settings) {
+                // Embedding into Settings is supported from SuperSU v1.85 and up
+                boolean supported = false;
+                try {
+                    supported = (getPackageManager().getPackageInfo("eu.chainfire.supersu", 0).versionCode >= 185);
+                } catch (PackageManager.NameNotFoundException e) {
+                }
+                if (!supported) {
+                    target.remove(i);
+                }
             } else if (id == R.id.development_settings) {
                 if (!showDev) {
                     target.remove(i);
