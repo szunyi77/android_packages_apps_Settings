5e9c1fbe4d8a92b1529e922cc28eac57c59b3058
Danny Baumann <dannybaumann@web.de>
2014-07-01 10:23:37 +0200

    Clean up search code.
    Fix up coding style and add a few missing TypedArray.recycle() calls.
    Also make filtering code more efficient.
    
    Change-Id: Ie3824b41ae40010686ff3cb6cab7f12d19843834
    
    Conflicts:
    	src/com/android/settings/Settings.java

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -51,7 +51,6 @@ import android.os.UserManager;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
-import android.support.v4.view.MenuItemCompat;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -158,7 +157,6 @@ public class Settings extends PreferenceActivity
     private static final String SAVE_KEY_PARENT_HEADER = "com.android.settings.PARENT_HEADER";
 
     private static final String VOICE_WAKEUP_PACKAGE_NAME = "com.cyanogenmod.voicewakeup";
-    public static final String NOTIFIER_EXTRA = "notifier";
 
     static final int DIALOG_ONLY_ONE_HOME = 1;
 
@@ -243,9 +241,8 @@ public class Settings extends PreferenceActivity
         final InputMethodManager imm =
                 (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
 
-        mSearchBar = (SettingsAutoCompleteTextView) MenuItemCompat.getActionView(mSearchItem);
-
-        MenuItemCompat.setOnActionExpandListener(mSearchItem, new MenuItemCompat.OnActionExpandListener() {
+        mSearchBar = (SettingsAutoCompleteTextView) mSearchItem.getActionView();
+        mSearchItem.setOnActionExpandListener(new MenuItem.OnActionExpandListener() {
             @Override
             public boolean onMenuItemActionCollapse(MenuItem item) {
                 mSearchBar.clearFocus();
@@ -278,8 +275,7 @@ public class Settings extends PreferenceActivity
             AsyncTask<Void, Void, ArrayList<SearchInfo>> {
         @Override
         protected ArrayList<SearchInfo> doInBackground(Void... param) {
-            final ArrayList<SearchInfo> infos = SearchPopulator.getTitles(Settings.this);
-            return infos;
+            return SearchPopulator.loadSearchData(Settings.this);
         }
 
         @Override
@@ -307,9 +303,8 @@ public class Settings extends PreferenceActivity
             getWindow().setUiOptions(getIntent().getIntExtra(EXTRA_UI_OPTIONS, 0));
         }
 
-        Intent i = new Intent(this, SearchPopulator.class);
-        i.putExtra(NOTIFIER_EXTRA, new SearchNotifier(new Handler()));
-        startService(i);
+        startPopulatingSearchData();
+
         mActionBar = getActionBar();
         mActionBar.setDisplayShowCustomEnabled(true);
 
@@ -396,11 +391,9 @@ public class Settings extends PreferenceActivity
 
     @Override
     public void onBackPressed() {
-        if (mSearchBar != null) {
-            if (mSearchBar.hasFocus()) {
-                mSearchBar.clearFocus();
-                return;
-            }
+        if (mSearchBar != null && mSearchBar.hasFocus()) {
+            mSearchBar.clearFocus();
+            return;
         }
         super.onBackPressed();
     }
@@ -429,6 +422,7 @@ public class Settings extends PreferenceActivity
         mDevelopmentPreferencesListener = null;
     }
 
+    @Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
         mSearchBar.clearFocus();
@@ -584,6 +578,12 @@ public class Settings extends PreferenceActivity
                 }
             }
         }
+    }
+
+    private void startPopulatingSearchData() {
+        Intent i = new Intent(this, SearchPopulator.class);
+        i.putExtra(SearchPopulator.EXTRA_NOTIFIER, new SearchNotifier(new Handler()));
+        startService(i);
     }
 
     @Override
@@ -956,8 +956,7 @@ public class Settings extends PreferenceActivity
     }
 
     @Override
-    public void onItemClick(AdapterView<?> parent,
-                            View view, int position, long l) {
+    public void onItemClick(AdapterView<?> parent, View view, int position, long l) {
         SearchInfo info = (SearchInfo) parent.getItemAtPosition(position);
         mSearchBar.setText("");
         mSearchBar.clearFocus();
