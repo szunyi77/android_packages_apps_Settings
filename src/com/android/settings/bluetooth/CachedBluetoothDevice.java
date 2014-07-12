fab1bfb305f1482524faac07c26df5e69d9fe2f0
Altaf-Mahdi <altaf.mahdi@gmail.com>
2014-07-12 16:32:48 +0100

    Revert "roll back all bluetooth changes"
    This reverts commit 72ef7a5389150385686914a47ea11eb2d03ed446.

--- src/com/android/settings/bluetooth/CachedBluetoothDevice.java
+++ src/com/android/settings/bluetooth/CachedBluetoothDevice.java
@@ -64,6 +64,8 @@ final class CachedBluetoothDevice implements Comparable<CachedBluetoothDevice> {
 
     private boolean mVisible;
 
+    private boolean mDeviceRemove;
+
     private int mPhonebookPermissionChoice;
 
     private int mMessagePermissionChoice;
@@ -335,8 +337,10 @@ final class CachedBluetoothDevice implements Comparable<CachedBluetoothDevice> {
                 final boolean successful = dev.removeBond();
                 if (successful) {
                     if (Utils.D) {
+                        mDevice.setAlias(null);
                         Log.d(TAG, "Command sent successfully:REMOVE_BOND " + describe(null));
                     }
+                    setRemovable(true);
                 } else if (Utils.V) {
                     Log.v(TAG, "Framework rejected command immediately:REMOVE_BOND " +
                             describe(null));
@@ -394,6 +398,14 @@ final class CachedBluetoothDevice implements Comparable<CachedBluetoothDevice> {
                 mName = mDevice.getAddress();
             } else {
                 mName = name;
+            }
+            dispatchAttributesChanged();
+        }
+    }
+    void setAliasName(String name) {
+        if (!mName.equals(name)) {
+            if (!TextUtils.isEmpty(name)) {
+                mName = name;
                 mDevice.setAlias(name);
             }
             dispatchAttributesChanged();
@@ -422,12 +434,22 @@ final class CachedBluetoothDevice implements Comparable<CachedBluetoothDevice> {
         return mVisible;
     }
 
+    boolean isRemovable () {
+        return mDeviceRemove;
+   }
+
+
     void setVisible(boolean visible) {
         if (mVisible != visible) {
             mVisible = visible;
             dispatchAttributesChanged();
         }
     }
+
+    void setRemovable(boolean removable) {
+        mDeviceRemove = removable;
+    }
+
 
     int getBondState() {
         return mDevice.getBondState();
@@ -536,26 +558,39 @@ final class CachedBluetoothDevice implements Comparable<CachedBluetoothDevice> {
     }
 
     void onBondingStateChanged(int bondState) {
-        if (bondState == BluetoothDevice.BOND_NONE) {
-            mProfiles.clear();
-            mConnectAfterPairing = false;  // cancel auto-connect
-            setPhonebookPermissionChoice(ACCESS_UNKNOWN);
-            setMessagePermissionChoice(ACCESS_UNKNOWN);
-            mPhonebookRejectedTimes = 0;
-            savePhonebookRejectTimes();
-            mMessageRejectedTimes = 0;
-            saveMessageRejectTimes();
-        }
 
-        refresh();
+        if(DEBUG) Log.d(TAG, "onBondingStateChanged" + bondState);
 
-        if (bondState == BluetoothDevice.BOND_BONDED) {
-            if (mDevice.isBluetoothDock()) {
-                onBondingDockConnect();
-            } else if (mConnectAfterPairing) {
-                connect(false);
-            }
-            mConnectAfterPairing = false;
+        switch (bondState) {
+            case BluetoothDevice.BOND_NONE:
+                mProfiles.clear();
+                mConnectAfterPairing = false;  // cancel auto-connect
+                // fall through
+
+            case BluetoothDevice.BOND_BONDING:
+                //Sometimes Remote device is unpaired by itself & try to connect again.
+                //so permission should be reset for that particular device.
+                setPhonebookPermissionChoice(ACCESS_UNKNOWN);
+                setMessagePermissionChoice(ACCESS_UNKNOWN);
+                mPhonebookRejectedTimes = 0;
+                savePhonebookRejectTimes();
+                mMessageRejectedTimes = 0;
+                saveMessageRejectTimes();
+
+                refresh();
+                break;
+
+            case BluetoothDevice.BOND_BONDED:
+                if (mDevice.isBluetoothDock()) {
+                    onBondingDockConnect();
+                } else if (mConnectAfterPairing) {
+                    connect(false);
+                }
+                mConnectAfterPairing = false;
+                break;
+
+            default:
+                Log.e(TAG, "Incorrect Bond State received");
         }
     }
 
